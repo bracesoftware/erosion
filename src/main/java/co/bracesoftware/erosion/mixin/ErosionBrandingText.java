af347dfe161a95c6b4604d025d22f4cfdc584fc1
Tractus Corticospinalis <zukanoviceltarik@gmail.com>
2026-09-16 19:28:53 +0200

    update

--- src/main/java/co/bracesoftware/erosion/mixin/ErosionBrandingText.java
+++ src/main/java/co/bracesoftware/erosion/mixin/ErosionBrandingText.java
@@ -1,30 +1,34 @@
 package co.bracesoftware.erosion.mixin;
 
+import java.util.ArrayList;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import com.google.common.collect.ImmutableList;
 import co.bracesoftware.erosion.Erosion;
 import co.bracesoftware.erosion.ErosionExceptions.ErosionMixinException;
 import net.neoforged.neoforge.internal.BrandingControl;
 
+import java.util.List;
+
 @Mixin(value = BrandingControl.class, remap = false)
 public class ErosionBrandingText
 {
-    //credits modernfix dev for the @Inject,
-    //tried injecting into getBranding but didnt work
-    @Inject(
-        method = {"computeBranding"},
-        at = {@At(value = "INVOKE",
-        target = "Lnet/neoforged/fml/ModList;get()Lnet/neoforged/fml/ModList;")},
-        locals = LocalCapture.CAPTURE_FAILHARD, require = 1
-    )
-    private static void idkWhatToCallThis(
-        CallbackInfo ci, ImmutableList.Builder<String> builder
-    )
+    private static List<String> cachedBranding = null;
+
+    @Inject(method = "getBranding", at = @At("RETURN"), cancellable = true)
+    private static void onGetBranding(CallbackInfoReturnable<List<String>> cir)
     {
-        builder.add(Erosion.MOD_BRANDING);
+        if(cachedBranding == null)
+        {
+            List<String> list = new ArrayList<>(cir.getReturnValue());
+            list.add(Erosion.MOD_BRANDING);
+            cachedBranding = List.copyOf(list);
+        }
+        cir.setReturnValue(cachedBranding);
     }
 }
