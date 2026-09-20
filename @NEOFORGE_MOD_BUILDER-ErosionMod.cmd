4243197f8863eb074fc13c2b0be68990324b17f1
Tractus Corticospinalis <zukanoviceltarik@gmail.com>
2026-09-20 17:47:50 +0200

    update

--- @NEOFORGE_MOD_BUILDER-ErosionMod.cmd
+++ @NEOFORGE_MOD_BUILDER-ErosionMod.cmd
@@ -2,19 +2,38 @@
 title Erosion Mod Builder
 :start
 cls
+
 echo Generating project version...
+title Erosion Mod Builder: Generating project version...
+
 g++ vermgr.cpp -o vermgr -v -static
 call vermgr
+
 echo Writing changes to the code...
-call pushtogithub
+title Writing changes to the code...
+
+call _PUSHCHANGES
+
 echo Generating data...
+title Erosion Mod Builder: Generating data...
+
 call gradlew clean build
 call gradlew clean processResources
 call gradlew runData -stacktrace --no-configuration-cache
+
+title Erosion Mod Builder: Building...
 echo Building...
+
 call gradlew build -stacktrace --no-configuration-cache
+
 echo Releasing...
-echo - To release click on @RELEASE
+title Erosion Mod Builder: Releasing...
+
+echo - To release click on _RELEASE
+echo - To push changes run _PUSHCHANGES
 echo ====== COMPLETE PROCESS FINISHED! ======
+
+title Erosion Mod Builder: Process finished.
+
 pause
 goto start
